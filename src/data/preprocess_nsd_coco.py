cca00131469e1d548af59c8f534592e85726ac14
Helena Balabin <helena.balabin@kuleuven.be>
2024-10-25 14:35:30 +0200

    Find the NSD VG overlap

--- src/data/preprocess_nsd_coco.py
+++ src/data/preprocess_nsd_coco.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import hydra
 import pandas as pd
+from datasets import load_dataset
 from dotenv import load_dotenv
 from loguru import logger
 from nsd_access import NSDAccess
@@ -37,20 +38,39 @@ def preprocess_nsd_coco(cfg: DictConfig) -> None:
 
     logger.info(f"Preprocessing NSD-COCO data from: {nsd_dir}")
 
-    # TODO: Implement preprocessing steps here
-    # For example
-    # - Load the data
-
     # Load the metadata file using nsd_access
     nsd = NSDAccess(nsd_dir)
-
     nsd_stim_desc = pd.read_csv(nsd.stimuli_description_file, index_col=0)
+    nsd_coco_ids = set(nsd_stim_desc["cocoId"].tolist())
 
-    # TODO either add the code or the preprocessed COCO metadata file
+    # Load the VG metadata
+    vg_metadata = load_dataset(
+        cfg.data.vg_metadata_hf_identifier,
+        cache_dir=cfg.data.cache_dir,
+        split="train",
+    )
+    # Convert to pandas DataFrame
+    vg_df = vg_metadata.to_pandas()
+    # Drop duplicates based on 'cocoid'
+    vg_df_unique = vg_df.drop_duplicates(subset=["cocoid"], keep="first")
+    # Rename the 'cocoid' column to 'cocoId'
+    vg_df_unique = vg_df_unique.rename(columns={"cocoid": "cocoId"})
+    # Get the COCO IDs from the VG metadata
+    vg_coco_ids = set(vg_df_unique["cocoId"])
 
-    # - Clean the data
-    # - Transform the data
-    # - Save the processed data
+    # Find the overlap between the NSD and VG COCO IDs
+    overlap_coco_ids = nsd_coco_ids & vg_coco_ids
+    logger.info(f"Found {len(overlap_coco_ids)} overlapping COCO IDs.")
+
+    # Merge the NSD and VG metadata on the COCO ID, make sure there are no extra indices
+    nsd_vg_metadata = nsd_stim_desc.merge(vg_df_unique, on="cocoId", how="inner")
+
+    # Create a new subdirectory in the NSD directory
+    nsd_vg_dir = nsd_dir / "nsd_vg"
+    nsd_vg_dir.mkdir(parents=True, exist_ok=True)
+
+    # Save the merged metadata to a CSV file
+    nsd_vg_metadata.to_csv(nsd_vg_dir / "nsd_vg_metadata.csv", index=False)
 
     logger.info("Preprocessing complete.")
 
